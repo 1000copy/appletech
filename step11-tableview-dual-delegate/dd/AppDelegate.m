71d1811fe73923465cb18acc8828baff3c50608a
1000copy <1000copy@gmail.com>
2015-10-08 17:31:55 +0800

    change nsarray to nsmutablearray

--- step11-tableview-dual-delegate/dd/AppDelegate.m
+++ step11-tableview-dual-delegate/dd/AppDelegate.m
@@ -11,6 +11,7 @@
 @interface AppDelegate (){
     NSMutableArray * states;
     NSMutableArray * citys;
+    NSMutableDictionary * citys_dict;
     NSInteger r;
 }
 
@@ -18,15 +19,31 @@
 @end
 
 @implementation AppDelegate
--(id)init{
-    self = [super init];
-    r = 0;
+-(void)init1{
     states = [[NSMutableArray alloc]initWithArray: @[@"CA",@"Washington"]];
     NSArray *citys1 = @[@"LA",@"SF"];
     NSArray *citys2 = @[@"Portland",@"Seatle"];
     citys = [[NSMutableArray alloc]initWithCapacity:10];
     [citys addObject:citys1];
     [citys addObject:citys2];
+
+}
+
+-(void)init2{
+    states = [[NSMutableArray alloc]initWithArray: @[@"CA",@"Washington"]];
+//    citys_dict = [[NSMutableDictionary alloc]initWithObjects:@[@[@"LA",@"SF",@"San Diego"],@[@"Portland",@"Seatle"]] forKeys:@[@"CA",@"Washington"]];
+    NSMutableArray *citys1 = [[NSMutableArray alloc]initWithArray:@[@"LA",@"SF"]];
+    NSMutableArray *citys2 = [[NSMutableArray alloc]initWithArray:@[@"Portland",@"Seatle"]];
+    citys_dict = [[NSMutableDictionary alloc]initWithObjects:@[citys1,citys2] forKeys:@[@"CA",@"Washington"]];
+    [[citys_dict objectForKey:@"CA"] addObject:@"San Diego"]; //cause SIGINT if content is NSArray ,but NSMutable is OK
+}
+
+
+-(id)init{
+    self = [super init];
+    r = 0;
+//    [self init1];
+    [self init2];
     return self;
 }
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
@@ -45,7 +62,8 @@
     if (tableView == self.tv1)
         return states.count;
     if (tableView == self.tv2){
-        NSArray * arr =(NSArray*)(citys[r]);
+//        NSArray * arr =(NSArray*)(citys[r]);
+        NSArray * arr =(NSArray*)([citys_dict objectForKey:[states objectAtIndex:r]]);
         return arr.count;
     }
     return 0;
@@ -56,7 +74,8 @@
         return [states objectAtIndex:row];
     }
     if (tableView == self.tv2){
-        return [[citys objectAtIndex:r] objectAtIndex:row];
+//        return [[citys objectAtIndex:r] objectAtIndex:row];
+        return [[citys_dict objectForKey:[states objectAtIndex:r] ]objectAtIndex:row];
     }return nil;
 }
 
