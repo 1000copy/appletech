bfc95194f007176868ef4911519be1f4b2f6a506
1000copy <1000copy@gmail.com>
2015-12-29 13:47:43 +0800

    auto layout

--- swift/nav/dou3/dou3/AppDelegate.swift
+++ swift/nav/dou3/dou3/AppDelegate.swift
@@ -8,6 +8,101 @@
 
 import UIKit
 
+// Naviagtion Back> button
+let MAINLABEL_TAG  = 1
+class MyCell : UITableViewCell{
+    var mainLabel : UILabel?
+        var mainLabel1 : UILabel?
+        var mainLabel2 : UILabel?
+    required override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style,reuseIdentifier: reuseIdentifier)
+
+        f2()
+    }
+    func f1(){
+        mainLabel = UILabel(frame:CGRectMake(0.0, 0.0, 220.0, 15.0))
+        mainLabel1 = UILabel(frame:CGRectMake(0.0, 20.0, 220.0, 15.0))
+        mainLabel2 = UILabel(frame:CGRectMake(0.0, 40.0, 220.0, 15.0))
+        mainLabel!.tag = MAINLABEL_TAG;
+        mainLabel!.textAlignment = .Left
+        mainLabel!.textColor = UIColor.blackColor()
+        self.contentView.addSubview(mainLabel!)
+        mainLabel1!.tag = MAINLABEL_TAG;
+        mainLabel1!.textAlignment = .Left
+        mainLabel1!.textColor = UIColor.blackColor()
+        self.contentView.addSubview(mainLabel1!)
+        mainLabel2!.tag = MAINLABEL_TAG;
+        mainLabel2!.textAlignment = .Left
+        mainLabel2!.textColor = UIColor.blackColor()
+        self.contentView.addSubview(mainLabel2!)
+
+    }
+    func f2(){
+        mainLabel = UILabel()
+        mainLabel1 = UILabel()
+        mainLabel2 = UILabel()
+        self.contentView.addSubview(mainLabel!)
+        self.contentView.addSubview(mainLabel1!)
+        self.contentView.addSubview(mainLabel2!)
+        mainLabel!.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel1!.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel2!.translatesAutoresizingMaskIntoConstraints = false
+        
+        let h1 = NSLayoutConstraint(item: mainLabel!, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 5)
+        self.addConstraint(h1)
+        
+        let v1 = NSLayoutConstraint(item: mainLabel!, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 5)
+        self.addConstraint(v1)
+
+        let h2 = NSLayoutConstraint(item: mainLabel1!, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem:mainLabel!, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 5)
+        self.addConstraint(h2)
+        
+        let v2 = NSLayoutConstraint(item: mainLabel1!, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 5)
+        self.addConstraint(v2)
+        
+        let h3 = NSLayoutConstraint(item: mainLabel2!, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem:mainLabel1!, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 5)
+        self.addConstraint(h3)
+        
+        let v3 = NSLayoutConstraint(item: mainLabel2!, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 5)
+        self.addConstraint(v3)
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+    }
+}
+class Page6: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 90
+    }
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "section"
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let MyIdentifier = "MyCell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(MyIdentifier)
+        if (cell == nil){
+            cell = MyCell(style: .Default, reuseIdentifier: MyIdentifier)
+        }
+        (cell as! MyCell).mainLabel?.text = "text1"
+        (cell as! MyCell).mainLabel1?.text = "text2"
+        (cell as! MyCell).mainLabel2?.text = "text3"
+        return cell!;
+        
+    }
+}
+
+
 class Page1 : UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +116,8 @@ class Page1 : UIViewController{
 
     }
     func buttonAction(sender:UIButton!){
-        let p2 = Page2(nibName: nil, bundle: nil)
+//        let p2 = Page2(nibName: nil, bundle: nil)
+        let p2 = Page6(nibName: nil, bundle: nil)
         self.navigationController?.pushViewController(p2, animated: true)
 //        navigationController?.navigationBar.topItem?.title = "page 2"
     }
@@ -58,6 +154,102 @@ class Page2 : UIViewController{
     }
 }
 
+class Page3 : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let newView = UIView()
+        newView.backgroundColor = UIColor.redColor()
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newView)
+        
+        let views = ["view": view, "newView": newView]
+        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[view]-(<=0)-[newView(100)]", options: NSLayoutFormatOptions.AlignAllCenterY, metrics: nil, views: views)
+        view.addConstraints(horizontalConstraints)
+        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[view]-(<=0)-[newView(100)]", options: NSLayoutFormatOptions.AlignAllCenterX, metrics: nil, views: views)
+        view.addConstraints(verticalConstraints)
+    }
+}
+class Page4 : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let newView = UIView()
+        newView.backgroundColor = UIColor.redColor()
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newView)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+        view.addConstraint(horizontalConstraint)
+        
+        let verticalConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+        view.addConstraint(verticalConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraint(widthConstraint)
+        
+        let heightConstraint = NSLayoutConstraint(item: newView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraint(heightConstraint)
+    }
+}
+
+class Red : UIView{
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+
+}
+class Page5 : UIViewController{
+    var mainView:UIView?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainView = UIView()
+        vvv(mainView!)
+        let v = UIView()
+        v3(v)
+    }
+    func vvv(v1:UIView){
+        v1.backgroundColor = UIColor.yellowColor()
+        v1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(v1)
+        let horizontalConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 5)
+        view.addConstraint(horizontalConstraint)
+        
+        let verticalConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 100)
+        view.addConstraint(verticalConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraint(widthConstraint)
+        
+        let heightConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraint(heightConstraint)
+    }
+    func v3(v1:UIView){
+        v1.backgroundColor = UIColor.redColor()
+        v1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(v1)
+        let horizontalConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: 5)
+        view.addConstraint(horizontalConstraint)
+        
+        let verticalConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 100)
+        view.addConstraint(verticalConstraint)
+        
+        let widthConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraint(widthConstraint)
+        
+        let heightConstraint = NSLayoutConstraint(item: v1, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraint(heightConstraint)
+    }
+
+}
+
+class RedBlock : UIView{
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
+
 class Nav : UINavigationController{
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,6 +259,8 @@ class Nav : UINavigationController{
 
     }
 }
+
+
 
 @UIApplicationMain
 
