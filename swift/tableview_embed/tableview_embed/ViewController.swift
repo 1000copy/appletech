66bcc5b6b6bd92151bae75567784653ac7ce1f81
1000copy <1000copy@gmail.com>
2016-02-05 10:02:26 +0800

    tableview insert ,3 ways

--- swift/tableview_embed/tableview_embed/ViewController.swift
+++ swift/tableview_embed/tableview_embed/ViewController.swift
@@ -29,12 +29,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let a  = LangTableViewController()
-        a.view.frame = CGRectMake(0,100,300,200)
+        a.view.frame = CGRectMake(0,100,300,100)
         self.view.addSubview(a.view)
         self.addChildViewController(a)
+        
+        let b  = LangTable()
+        b.frame = CGRectMake(0,200,300,100)
+        self.view.addSubview(b)
+        
+        let c  = LangTableWithDequeueReusableCellWithIdentifier()
+        c.frame = CGRectMake(0,300,300,100)
+        self.view.addSubview(c)
     }
 }
 
 
 
 
+class LangTable : UITableView,UITableViewDataSource{
+    let arr = ["java"]
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame:frame,style:style)
+        self.dataSource = self
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let a = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        a.textLabel?.text = String(arr[indexPath.row])
+        return a
+    }
+}
+
+class LangTableWithDequeueReusableCellWithIdentifier: UITableView,UITableViewDataSource{
+    let arr = ["javascript","delphi"]
+    let MyIdentifier = "cell"
+    override init(frame: CGRect, style: UITableViewStyle) {
+        super.init(frame:frame,style:style)
+        self.dataSource = self
+        self.registerClass(UITableViewCell.self, forCellReuseIdentifier: MyIdentifier)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let a = tableView.dequeueReusableCellWithIdentifier(MyIdentifier)!
+        a.textLabel?.text = String(arr[indexPath.row])
+        return a
+    }
+}
