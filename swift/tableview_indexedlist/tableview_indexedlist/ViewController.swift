8b35a434746942de35d05f14a4e0ba315b8c7507
1000copy <1000copy@gmail.com>
2016-02-12 21:28:07 +0800

    init: indexed list tableview swift example

---
+++ swift/tableview_indexedlist/tableview_indexedlist/ViewController.swift
@@ -0,0 +1,155 @@
+//
+//  ViewController.swift
+//  tableview_indexedlist
+//
+//  Created by quota on 2/12/16.
+//  Copyright © 2016 liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    class User: NSObject {
+        let name: String
+        var section: Int?
+        
+        init(name: String) {
+            self.name = name
+        }
+    }
+    
+    // custom type to represent table sections
+    class Section {
+        var users: [User] = []
+        
+        func addUser(user: User) {
+            self.users.append(user)
+        }
+    }
+    
+    // raw user data
+    let names = [
+        "Clementine",
+        "Bessie",
+        "Yolande",
+        "Tynisha",
+        "Ellyn",
+        "Trudy",
+        "Fredrick",
+        "Letisha",
+        "Ariel",
+        "Bong",
+        "Jacinto",
+        "Dorinda",
+        "Aiko",
+        "Loma",
+        "Augustina",
+        "Margarita",
+        "Jesenia",
+        "Kellee",
+        "Annis",
+        "Charlena"
+    ]
+    
+    // `UIKit` convenience class for sectioning a table
+    let collation = UILocalizedIndexedCollation.currentCollation()
+        as UILocalizedIndexedCollation
+    
+    // table sections
+    var sections: [Section] {
+        // return if already initialized
+        if self._sections != nil {
+            return self._sections!
+        }
+        
+        // create users from the name list
+        var users: [User] = names.map { name in
+            var user = User(name: name)
+            user.section = self.collation.sectionForObject(user, collationStringSelector: "name")
+            return user
+        }
+        
+        // create empty sections
+        var sections = [Section]()
+        for i in 0..<self.collation.sectionIndexTitles.count {
+            sections.append(Section())
+        }
+        
+        // put each user in a section
+        for user in users {
+            sections[user.section!].addUser(user)
+        }
+        
+        // sort each section
+        for section in sections {
+            section.users = self.collation.sortedArrayFromArray(section.users, collationStringSelector: "name") as! [User]
+        }
+        
+        self._sections = sections
+        
+        return self._sections!
+        
+    }
+    var _sections: [Section]?
+    
+    // table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView)
+        -> Int {
+            return self.sections.count
+    }
+    
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int)
+        -> Int {
+            return self.sections[section].users.count
+    }
+    
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath)
+        -> UITableViewCell {
+            let user = self.sections[indexPath.section].users[indexPath.row]
+            
+//            let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as UITableViewCell
+            let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+            
+            cell.textLabel!.text = user.name
+            return cell
+    }
+    
+    /* section headers
+    appear above each `UITableView` section */
+    override func tableView(tableView: UITableView,
+        titleForHeaderInSection section: Int)
+        -> String {
+            // do not display empty `Section`s
+            if !self.sections[section].users.isEmpty {
+                return self.collation.sectionTitles[section] as String
+            }
+            return ""
+    }
+    
+    /* section index titles
+    displayed to the right of the `UITableView` */
+    override func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]? {
+         return self.collation.sectionIndexTitles
+    }
+  
+    override func tableView(tableView: UITableView,
+        sectionForSectionIndexTitle title: String,
+        atIndex index: Int)
+        -> Int {
+            return self.collation.sectionForSectionIndexTitleAtIndex(index)
+    }
+}
+
