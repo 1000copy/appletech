bfc1fcd5e8948ea192dc9c1c06a5a5a998661275
1000copy <1000copy@gmail.com>
2017-03-16 16:33:03 +0800

    StackView OK

--- swift/view/views/views/AppDelegate.swift
+++ swift/view/views/views/AppDelegate.swift
@@ -9,20 +9,100 @@
                 return true
             }
         }
-        class Page: UIViewController{
-            var vw : UIView!
+        class Page: UIViewController {
             override func viewDidLoad() {
                 super.viewDidLoad()
-                vw = UIView()
-                vw.backgroundColor = .red
-                view.addSubview(vw)
-                vw.translatesAutoresizingMaskIntoConstraints = false
-                let horizontalConstraint = NSLayoutConstraint(item: vw, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.right, multiplier: 1, constant: -5)
-                let verticalConstraint = NSLayoutConstraint(item: vw, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: -5)
-                let widthConstraint = NSLayoutConstraint(item: vw, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
-                let heightConstraint = NSLayoutConstraint(item: vw, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 50)
-                view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
-                
+                var verticalStackView: UIStackView!
+                verticalStackView = UIStackView()
+                verticalStackView.axis = .vertical
+                verticalStackView.distribution = .fillEqually
+                verticalStackView.spacing = 10
+                let blue = UILabel()
+                blue.backgroundColor = .blue
+                let green = UILabel()
+                green.backgroundColor = .green
+                let horizontalStackView = UIStackView()
+                horizontalStackView.axis = .horizontal
+                horizontalStackView.distribution = .fillEqually
+                horizontalStackView.spacing = 10
+                horizontalStackView.addArrangedSubview(blue)
+                horizontalStackView.addArrangedSubview(green)
+                let red = UILabel()
+                red.backgroundColor = .red
+                red.heightAnchor.constraint(equalToConstant: 34).isActive = true
+                verticalStackView.addArrangedSubview(horizontalStackView)
+                verticalStackView.addArrangedSubview(red)
+                verticalStackView.frame = view.bounds
+                view.addSubview(verticalStackView)
             }
         }
+//        greenImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//        blueImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+
+class Page101: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let s   = UIStackView()
+        s.translatesAutoresizingMaskIntoConstraints = false;
         
+        s.axis  = .vertical
+        s.spacing   = 6
+        
+        let label1 = UILabel()
+        label1.backgroundColor = UIColor.blue
+        label1.text  = "Hallo"
+        let label2 = UILabel()
+        label2.backgroundColor = UIColor.yellow
+        label2.text  = "Hi"
+        let label3 = UILabel()
+        label3.backgroundColor = UIColor.red
+        label3.text  = "Hello"
+        let stackView   = UIStackView()
+        stackView.axis  = .horizontal
+        stackView.spacing   = 6
+        stackView.addArrangedSubview(label1)
+        stackView.addArrangedSubview(label2)
+        stackView.addArrangedSubview(label3)
+        stackView.translatesAutoresizingMaskIntoConstraints = false;
+        s.addArrangedSubview(stackView)
+        
+        let label21 = UILabel()
+        label21.backgroundColor = UIColor.blue
+        label21.text  = "Hallo"
+        let label22 = UILabel()
+        label22.backgroundColor = UIColor.yellow
+        label22.text  = "Hi"
+        let label23 = UILabel()
+        label23.backgroundColor = UIColor.red
+        label23.text  = "Hello"
+        let stackView2   = UIStackView()
+        stackView2.axis  = .horizontal
+        stackView2.spacing   = 6
+        stackView2.addArrangedSubview(label21)
+        stackView2.addArrangedSubview(label22)
+        stackView2.addArrangedSubview(label23)
+        stackView2.translatesAutoresizingMaskIntoConstraints = false;
+        s.addArrangedSubview(stackView2)
+        
+        let label31 = UILabel()
+        label31.backgroundColor = UIColor.blue
+        label31.text  = "Hallo"
+        let label32 = UILabel()
+        label32.backgroundColor = UIColor.yellow
+        label32.text  = "Hi"
+        let label33 = UILabel()
+        label33.backgroundColor = UIColor.red
+        label33.text  = "Hello"
+        let stackView3   = UIStackView()
+        stackView3.axis  = .horizontal
+        stackView3.spacing   = 6
+        stackView3.addArrangedSubview(label31)
+        stackView3.addArrangedSubview(label32)
+        stackView3.addArrangedSubview(label33)
+        stackView3.translatesAutoresizingMaskIntoConstraints = false;
+        s.addArrangedSubview(stackView3)
+//        s.frame = view.bounds
+        view.addSubview(s)
+        
+    }
+}
