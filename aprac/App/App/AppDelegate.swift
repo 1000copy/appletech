afc6e2ee3630cd649c2ddeda0643553cabead500
1000copy <1000copy@gmail.com>
2017-05-17 17:13:03 +0800

    aprac.iOS kick off1

--- aprac/App/App/AppDelegate.swift
+++ aprac/App/App/AppDelegate.swift
@@ -1,12 +1,41 @@
 import aprac
 @UIApplicationMain
 class App: AppBase {
-    override func main() -> Page{
-        return P()
+    override func main() -> Any{
+        let p = P()
+        let p2 = P2()
+        let p3 = P3()
+        return Nav([p,p2,p3])
     }
 }
 class P : Page{
-    override func viewDidLoad() {
+    override func loaded() {
         view.backgroundColor = .green
+        let bgImage = UIImageView()
+        bgImage.frame = CGRect(x: 0,y: 0,width: 100,height: 100)
+        bgImage.image = Image.block(UIColor.red)
+        self.view.addSubview(bgImage)
     }
 }
+class P2 : Page{
+    override func loaded() {
+        view.backgroundColor = .blue
+        let bgImage = UIImageView()
+        bgImage.frame = CGRect(x: 0,y: 0,width: 100,height: 100)
+        bgImage.image = Image.block(UIColor.red)
+        self.view.addSubview(bgImage)
+    }
+}
+class P3 : Page{
+    override func loaded() {
+        view.backgroundColor = .red
+        let bgImage = UIImageView()
+        bgImage.frame = CGRect(x: 0,y: 0,width: 100,height: 100)
+        bgImage.image = Image.block(UIColor.red)
+        self.view.addSubview(bgImage)
+    }
+}
+//class N : Nav{
+//    override func loaded() {
+//    }
+//}
--- aprac/aprac/aprac/main.swift
+++ aprac/aprac/aprac/main.swift
@@ -1,31 +1,67 @@
  import UIKit
  open class Page : UIViewController{
+    open func loaded(){
     
+    }
+    open override func viewDidLoad() {
+        loaded()
+    }
  }
  open  class AppBase: UIResponder, UIApplicationDelegate {
     public var window: UIWindow?
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
         run()
-        MainPage = main()
+        MainPage = main() as! UIViewController
         return true
     }
-    open  func run(){
+    func run(){
         window = UIWindow()
         window!.makeKeyAndVisible()
     }
-    open  func main() -> Page{
+    open  func main() -> Any{
         return Page()
     }
-    
-    open var MainPage : UIViewController {
+    var MainPage : UIViewController {
         get{
             return window!.rootViewController!
         }
         set (page){
             window!.rootViewController = page
-//            window!.rootViewController!.view.backgroundColor = .blue
         }
     }
     
+ }
+ public class Image : UIImage{
+    public class func block(_ color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 10.0,height: 10.0 )
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+ }
+ 
+ public class Nav: UINavigationController {
+    var count = 0
+    var pages : [Page] = []
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    public init(_ pages : [Page]) {
+        self.pages = pages
+        super.init(rootViewController:pages[0])
+    }
+    open func loaded(){
+        
+    }
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+//        self.pushViewController(pages[0], animated: true)
+        loaded()
+    }
  }
