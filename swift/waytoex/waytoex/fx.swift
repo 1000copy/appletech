3e9049938509e0c201964ac3b264142f3c79d7d3
1000copy <1000copy@gmail.com>
2017-06-06 18:40:40 +0800

    rewrite v2ex BEGIN

--- swift/waytoex/waytoex/fx.swift
+++ swift/waytoex/waytoex/fx.swift
@@ -1,9 +1,50 @@
-//
-//  fx.swift
-//  waytoex
-//
-//  Created by lcj on 2017/6/6.
-//  Copyright © 2017年 lcj. All rights reserved.
-//
+import UIKit
 
-import Foundation
+typealias  OnCellData = (_ cell : UITableViewCell ,_ obj : Any)->Void
+class Table: UITableView,UITableViewDataSource,UITableViewDelegate{
+    var ds_ : [[Any]]?
+    var onCellData_ : OnCellData?
+    var onCellData : OnCellData?{
+        get{return onCellData_}
+        set{
+            onCellData_ = newValue
+        }
+    }
+    public var  ds : [[Any]]?{
+        get{return ds_}
+        set{
+            ds_ = newValue
+        }
+    }
+    var  cellClass_ : AnyClass?
+    public var  cellClass : AnyClass?{
+        get{return cellClass_}
+        set{cellClass_ = newValue}
+    }
+    func load(){
+        self.dataSource = self
+        self.delegate = self
+        register( cellClass, forCellReuseIdentifier: "\(cellClass)");
+        reloadData()
+        print("\(cellClass)")
+    }
+    func numberOfSections(in: UITableView) -> Int {
+        return ds!.count
+    }
+    // 就说怎么cell上面有空白？调了很久
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 44
+//    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ds![section].count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let arr = ds!
+        let a  = dequeueReusableCell(withIdentifier: "\(cellClass)", for: indexPath) ;
+        loadCell(a,arr[indexPath.section][indexPath.row])
+        return a
+    }
+    func loadCell(_ cell : UITableViewCell,_ item : Any){
+        onCellData_?(cell,item)
+    }
+}
